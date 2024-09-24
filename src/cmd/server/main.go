01025282d6dfeaf54de4ca1ba399a040d3438a88
Yusuke_Saito <info@ysaito.dev>
2024-09-25 05:06:13 +0900

    done ch05

---
+++ src/cmd/server/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"os/signal"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
+
+	hellopb "mygrpc/pkg/grpc"
+)
+
+type myServer struct {
+	hellopb.UnimplementedGreetingServiceServer
+}
+
+func (s *myServer) Hello(ctx context.Context, req *hellopb.HelloRequest) (*hellopb.HelloResponse, error) {
+	return &hellopb.HelloResponse{
+		Message: fmt.Sprintf("hello, %s!", req.GetName()),
+	}, nil
+}
+
+func NewMyServer() *myServer {
+	return &myServer{}
+}
+
+func main() {
+	port := 8080
+	listener, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+
+	s := grpc.NewServer()
+
+	hellopb.RegisterGreetingServiceServer(s, NewMyServer())
+
+	reflection.Register(s)
+
+	go func() {
+		log.Printf("Starting gRPC server on :%d", port)
+		s.Serve(listener)
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, os.Interrupt)
+	<-quit
+	log.Println("Shutting down gRPC server...")
+	s.GracefulStop()
+}
