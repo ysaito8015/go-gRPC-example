80b18282c0ac7f9f84d6a319dc817050053def99
Yusuke_Saito <info@ysaito.dev>
2024-09-26 21:10:52 +0900

    Implement Server Stream RPC for client

--- src/cmd/client/main.go
+++ src/cmd/client/main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"bufio"
 	"context"
+	"errors"
 	"fmt"
+	"io"
 	"log"
 	"os"
 
@@ -39,7 +41,8 @@ func main() {
 
 	for {
 		fmt.Println("1: send Request")
-		fmt.Println("2: exit")
+		fmt.Println("2: HelloServerStream")
+		fmt.Println("3: exit")
 		fmt.Print("please enter the number: ")
 
 		scanner.Scan()
@@ -49,11 +52,40 @@ func main() {
 		case "1":
 			Hello()
 		case "2":
+			HelloServerStream()
+		case "3":
 			fmt.Println("bye.")
 			goto M
 		}
 	}
 M:
+}
+
+func HelloServerStream() {
+	fmt.Print("Please enter your name: ")
+	scanner.Scan()
+	name := scanner.Text()
+
+	req := &hellopb.HelloRequest{
+		Name: name,
+	}
+	stream, err := client.HelloServerStream(context.Background(), req)
+	if err != nil {
+		fmt.Println("failed to call HelloServerStream: ", err)
+		return
+	}
+
+	for {
+		res, err := stream.Recv()
+		if errors.Is(err, io.EOF) {
+			fmt.Println("all the responses have already received.")
+			break
+		}
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(res)
+	}
 }
 
 func Hello() {
